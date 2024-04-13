7227b0abcc0e799a7cfe8154d88b898c3cc49725
lasse <77295879+lassejlv@users.noreply.github.com>
2024-04-13 10:45:11 +0200

    Update cors.ts

--- api/src/middleware/cors.ts
+++ api/src/middleware/cors.ts
@@ -1,7 +1,7 @@
 import { env } from "../env";
 
 export async function cors(req: Request, props: any): Promise<void> {
-  // Allow requests from any origin
+  
   props.headers.set("Access-Control-Allow-Origin", env.CORS_ORIGIN);
 
   return;
